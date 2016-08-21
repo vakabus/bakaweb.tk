01bfd50fb76783ec4c68d7204d463dc4f28e22f3
Vaclav Sraier <vasek@sraier.cz>
2016-08-21 17:28:49 +0200

    added autocompletition of https:// beggining to login url

--- bakalari/forms.py
+++ bakalari/forms.py
@@ -3,6 +3,6 @@ from django.forms import URLInput, PasswordInput, TextInput
 
 
 class LoginForm(forms.Form):
-    url = forms.CharField(max_length=256, label='', widget=URLInput(attrs={'class': 'pure-u-1 pure-u-md-1-4', 'placeholder': 'URL Bakalářů'}))
+    url = forms.CharField(max_length=256, label='', widget=URLInput(attrs={'id': 'login_url', 'class': 'pure-u-1 pure-u-md-1-4', 'placeholder': 'URL Bakalářů'}))
     username = forms.CharField(max_length=32, label='', widget=TextInput(attrs={'class': 'pure-u-1 pure-u-md-1-4', 'placeholder': 'Jméno'}))
     password = forms.CharField(max_length=128, label='', widget=PasswordInput(attrs={'class': 'pure-u-1 pure-u-md-1-4', 'placeholder': 'Heslo'}))
