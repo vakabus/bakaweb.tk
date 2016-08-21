eb3854342f7d06f50031e482f49d6580906c09c6
Vaclav Sraier <vasek@sraier.cz>
2016-08-21 13:26:39 +0200

    changes in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 sudo systemctl stop schooltools.service
 cd "/home/schooltools/pyschooltools"
 git pull
+python3 manage.py migrate
+chmod 0660 db.sqlite3
 sudo pip3 install --upgrade https://github.com/vakabus/pybakalib/archive/master.zip
 sudo systemctl start schooltools.service
