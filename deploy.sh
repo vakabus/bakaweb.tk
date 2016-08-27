70ccc7a38708567593b2eca81388fa28a7483169
Vaclav Sraier <vasek@sraier.cz>
2016-08-27 20:37:01 +0200

    updated deploy script to handle automatic news check

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 sudo systemctl stop schooltools.service
+sudo systemctl stop schooltools_check.service
+sudo systemctl stop schooltools_check.timer
 cd "/home/schooltools/pyschooltools"
 git pull
 python3 manage.py migrate
 chmod 0660 db.sqlite3
 sudo pip3 install --upgrade https://github.com/vakabus/pybakalib/archive/master.zip
 sudo systemctl start schooltools.service
+sudo systemctl start schooltools_check.timer
