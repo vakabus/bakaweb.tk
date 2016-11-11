e17c67bbcbb30d9eb2b96c677f4671161a811f5b
Vasek Sraier <vasek@sraier.cz>
2016-11-11 18:42:31 +0100

    another attempt

--- SchoolTools/settings.py
+++ SchoolTools/settings.py
@@ -123,6 +123,22 @@ CACHES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
