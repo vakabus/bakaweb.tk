42b1ba198c92875b73e3c6b4f65f239aeba31715
Vasek Sraier <git@vakabus.cz>
2017-05-04 11:49:13 +0200

    removed filter for 5.A, 5.B, 1.G

--- bakalari/management/commands/check_news.py
+++ bakalari/management/commands/check_news.py
@@ -84,8 +84,6 @@ class Command(BaseCommand):
                     if n.date < subscription.last_check:
                         break
                     try:
-                        if '5.A' in n.title or '5.B' in n.title or '1.G' in n.title:
-                            continue
                         notify(client, subscription, n)
                     except RequestException:
                         logger.info('Failed to send notification...')
