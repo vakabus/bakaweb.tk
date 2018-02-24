1432c73201e4713101f96672d09c55dc7a0ecb74
Vasek Sraier <git@vakabus.cz>
2018-02-24 22:22:31 +0100

    updated order for checking news

--- bakalari/management/commands/check_news.py
+++ bakalari/management/commands/check_news.py
@@ -65,7 +65,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.info('[BAKANEWS CHECK STARTED]')
-        subscriptions = NotificationSubscription.objects.all()
+        subscriptions = NotificationSubscription.objects.order_by('-last_seen')
         for subscription in subscriptions:
             logger.info('Checking news for {}'.format(subscription.name))
             try:
