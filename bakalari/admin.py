f8acc9548071cb77200abd3f1fb0a62765e061bc
Vasek Sraier <git@vakabus.cz>
2018-02-28 11:36:39 +0100

    added filters

--- bakalari/admin.py
+++ bakalari/admin.py
@@ -5,13 +5,16 @@ from bakalari.models import NotificationSubscription, LogSubject2, LogUser2
 
 class NotificationSubscriptionAdmin(admin.ModelAdmin):
     list_display = ('name', 'last_check', 'contact_type', 'contact_id', 'url', 'failed_checks')
+    list_filter = ('name', 'last_check', 'contact_type', 'contact_id', 'url', 'failed_checks')
 
 
 class LogUser2Admin(admin.ModelAdmin):
     list_display = ('user_id', 'school', 'dashboard_count', 'subject_count', 'last_seen')
+    list_filter = ('user_id', 'school', 'dashboard_count', 'subject_count', 'last_seen')
 
 class LogSubject2Admin(admin.ModelAdmin):
     list_display = ('subject', 'count')
+    list_filter = ('subject', 'count')
 
 admin.site.register(NotificationSubscription, NotificationSubscriptionAdmin)
 admin.site.register(LogUser2, LogUser2Admin)
